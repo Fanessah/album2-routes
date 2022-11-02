d8967584dccc18c4e3fee7429a682a9da9da2daf
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-11-02 10:19:42 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import{ Routes,Route} from 'react-router-dom'
+import Home from './scenes/Home'
+import SingleAlbum from './scenes/SingleAlbum'
+import AddAlbum from './scenes/AddAlbum'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/albums/new" element={<AddAlbum/>}/>
+        <Route path="/albums/:albumId" element={<SingleAlbum/>}/>
+
+      </Routes>
     </div>
   );
 }
